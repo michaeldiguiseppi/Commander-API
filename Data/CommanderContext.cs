f1b5ccfc4db50e0038644d75e1cdd3b0eab7954a
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2021-05-08 01:23:27 -0600

    Update gitignore

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,16 @@
+﻿using Commander_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander_API.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+    }
+}
